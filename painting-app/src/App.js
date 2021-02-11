34bf46562b0a82989f5bc6076dd284ef6a072837
racross1 <racross1@gmail.com>
2021-02-11 14:58:30 -0800

    frontend added

---
+++ painting-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Canvas from './Canvas.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
