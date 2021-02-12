5e1b167a46a5f95dfb65f0fa5318d9bef4843481
racross1 <racross1@gmail.com>
2021-02-12 10:47:47 -0800

    post request for painting save

--- painting-app/src/Canvas.js
+++ painting-app/src/Canvas.js
@@ -6,23 +6,45 @@ import { Painterro } from 'painterro'
 export default class Canvas extends React.Component {
 
     
-
+//in render need to dynamically add user and topic id - right now just hardcoded to 2 for both
     render() {
 
         Painterro({
-            saveHandler: function (image, done) {
-              var formData = new FormData();
-              formData.append('image', image.asBlob());
-              // you can also pass suggested filename 
-              // formData.append('image', image.asBlob(), image.suggestedFileName());
-              var xhr = new XMLHttpRequest();
-              xhr.open('POST', 'http://127.0.0.1:3000/paintings', true);
-              xhr.onload = xhr.onerror = function () {
-                done(true);
-              };
-              xhr.send(formData);
-            }
-          }).show()
+          //   saveHandler: function (image, done) {
+          //     var formData = new FormData();
+          //     formData.append("image", image.asBlob());
+          //     formData.append("user_id", 2)
+          //     formData.append("topic_id", 2)
+          //     // you can also pass suggested filename 
+          //     // formData.append('image', image.asBlob(), image.suggestedFileName());
+          //     var xhr = new XMLHttpRequest();
+          //     xhr.open('POST', 'http://127.0.0.1:3000/paintings', true);
+          //     xhr.onload = xhr.onerror = function () {
+          //       done(true);
+          //     };
+          //     xhr.send(formData);
+          //   }
+          // }).show()
+          saveHandler: function (image, done) {
+            var formData = new FormData();
+            formData.append("image", image.asBlob(), image.suggestedFileName());
+            formData.append("user_id", 2)
+            formData.append("topic_id", 2)
+            // you can also pass suggested filename 
+            // formData.append('image', image.asBlob(), image.suggestedFileName());
+            fetch('http://127.0.0.1:3000/paintings', {
+              method: 'POST',
+              body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+              console.log('Success:', data);
+            })
+            .catch((error) => {
+              console.error('Error:', error);
+            });
+          }
+        }).show()
 
         return (
             <div> 
