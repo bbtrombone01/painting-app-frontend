9980bd699236a1166ba0f3405426c6fb16754ddd
dangnhon <dang.nhon12345@yahoo.com>
2021-02-12 10:42:58 -0800

    getting ready to merge

--- painting-app/src/App.js
+++ painting-app/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './Navbar.js'
 import Canvas from './Canvas.js'
+// import LeftSideBox from './containers/LeftSideBox';
+import Sidenav from './components/Sidenav';
 
 function App() {
 
   return (
     <div className="App">
       
-      <Canvas />
+      <Sidenav /> 
     </div>
   );
 }
--- painting-app/src/Navbar.js
+++ painting-app/src/Navbar.js
@@ -2,22 +2,22 @@ import React from 'react'
 
 export default class Navbar extends React.Component {
 
-    componentDidMount() {
-        fetch('http://localhost:3000/users', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            }, 
-            body: JSON.stringify({
-                user: {
-                    username: "dangnhon",
-                    password: 'monkey12345',
-                    tagline: "this is the first test"
-                }})
-        })
-        .then(resp => resp.json())
-        .then(test => console.log(test))
-    }
+    // componentDidMount() {
+    //     fetch('http://localhost:3000/users', {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         }, 
+    //         body: JSON.stringify({
+    //             user: {
+    //                 username: "dangnhon",
+    //                 password: 'monkey12345',
+    //                 tagline: "this is the first test"
+    //             }})
+    //     })
+    //     .then(resp => resp.json())
+    //     .then(test => console.log(test))
+    // }
 
     render () {
         return (
